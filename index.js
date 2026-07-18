b9654ec252b1f3786cadf3036c8601bd4be7c5f9
Artur Barreto Silva <Silva.arturbarreto@gmail.com>
2026-07-18 14:32:19 -0300

    primeira versão

--- index.js
+++ index.js
@@ -1,45 +1,45 @@
 
 function principal(){
 
-    let tipo = document.getElementById("opcoes").value;
+    let tipo = document.getElementById("opcoes").value
 
-    let areaMosaico = 0;
+    let areaMosaico = 0
 
     switch(tipo){
 
         case "TriEquilatero":
-            let lado = Number(document.getElementById("lado").value);
+            let lado = Number(document.getElementById("lado").value)
 
-            let mosaico1 = new Mosaico(lado);
-            areaMosaico = mosaico1.calcularTriEquilatero();
-        break;
+            let mosaico1 = new Mosaico(lado)
+            areaMosaico = mosaico1.calcularTriEquilatero()
+        break
 
         case "TriIsoceles":
-            let ladoI = Number(document.getElementById("lado").value);
-            let base = Number(document.getElementById("base").value);
+            let ladoI = Number(document.getElementById("lado").value)
+            let base = Number(document.getElementById("base").value)
 
-            let mosaico2 = new Mosaico(ladoI, base);
-            areaMosaico = mosaico2.calcularTriIsoceles();
+            let mosaico2 = new Mosaico(ladoI, base)
+            areaMosaico = mosaico2.calcularTriIsoceles()
         break;
 
         case "TriEscaleno":
-            let ladoA = Number(document.getElementById("ladoA").value);
-            let ladoB = Number(document.getElementById("ladoB").value);
-            let ladoC = Number(document.getElementById("ladoC").value);
+            let ladoA = Number(document.getElementById("ladoA").value)
+            let ladoB = Number(document.getElementById("ladoB").value)
+            let ladoC = Number(document.getElementById("ladoC").value)
 
-            let mosaico3 = new Mosaico(0, 0, ladoA, ladoB, ladoC);
-            areaMosaico = mosaico3.calcularTriEscaleno();
-        break;
+            let mosaico3 = new Mosaico(0, 0, ladoA, ladoB, ladoC)
+            areaMosaico = mosaico3.calcularTriEscaleno()
+        break
     }
 
-    let largura = Number(document.getElementById("larguraP").value);
-    let altura = Number(document.getElementById("alturaP").value);
+    let largura = Number(document.getElementById("larguraP").value)
+    let altura = Number(document.getElementById("alturaP").value)
 
-    let parede = new Parede(largura, altura);
+    let parede = new Parede(largura, altura)
 
-    let areaParede = parede.calcularArea();
-    let areaComPerda = parede.calcularAreaComPerda();
-    let quantidade = parede.calcularQuantidadePecas(areaMosaico);
+    let areaParede = parede.calcularArea()
+    let areaComPerda = parede.calcularAreaComPerda()
+    let quantidade = parede.calcularQuantidadePecas(areaMosaico)
 
     respostaP.innerHTML = `
         Área da parede: ${areaParede.toFixed(2)} m² <br>
